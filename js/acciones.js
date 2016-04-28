aff395c394c8658ed36f5ceaad38f28257d502ad
lopezfloresoscar <oscar3f.desaupoo@gmail.com>
2016-04-28 14:23:02 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e) {
     document.addEventListener('deviceready', function(){
 		$('#btnvibrar').on('tap', function(){
-			navigator.notificacion.vibrate(5000);
+			navigator.notificacion.vibrate(2000);
 		});
 });
 });
